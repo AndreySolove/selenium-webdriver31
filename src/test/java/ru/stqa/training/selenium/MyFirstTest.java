cb4df6479ee0399ba65a9fe1b0f57e86bb8d2275
AndreySolove <103129321+AndreySolove@users.noreply.github.com>
2022-04-06 18:16:20 +0300

    Add MyFirstTest

---
+++ src/test/java/ru/stqa/training/selenium/MyFirstTest.java
@@ -0,0 +1,33 @@
+package ru.stqa.training.selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+
+public class MyFirstTest {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @BeforeTest
+    public void start() {
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 10);
+    }
+
+    @Test
+    public void myFirstTest() {
+        driver.get("https://google.com");
+    }
+
+    @AfterTest
+    public void stop() {
+        driver.quit();
+    }
+}
