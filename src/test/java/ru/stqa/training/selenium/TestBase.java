d7cf67bfcbc2a6413ce4d25516c429141ecacf50
AndreySolove <andreisolovev1992@mail.ru>
2022-04-19 00:24:42 +0300

    Created a class for 6 lesson.

---
+++ src/test/java/ru/stqa/training/selenium/TestBase.java
@@ -0,0 +1,33 @@
+package ru.stqa.training.selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+import java.util.concurrent.TimeUnit;
+
+public class TestBase {
+
+    public WebDriver driver;
+    public WebDriverWait wait;
+
+    @BeforeTest
+    public void start() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+        driver = new ChromeDriver(options);
+        wait = new WebDriverWait(driver, 10);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+//        driver = new FirefoxDriver();
+    }
+
+    @AfterTest
+    public void stop() {
+        driver.quit();
+        driver = null;
+       }
+}
